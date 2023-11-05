6665651bb166c61467f3f3251a345aad832a767a
yk-a111y <yk19980811@163.com>
2023-11-05 23:16:08 +0800

    docs: 初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-red-500">
+    yk-sokoban
+    {{ map }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useMapStore } from './store/map';
+
+const map = useMapStore();
+</script>
+
+<style scoped>
+</style>
---
+++ src/store/map.ts
@@ -0,0 +1,5 @@
+import { defineStore } from 'pinia';
+
+export const useMapStore = defineStore('map', () => {
+  const map = [1, 2, 3];
+})
